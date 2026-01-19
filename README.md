1ffecf0f75c7bd10af58b5fb876c485be386f99c
Weizenfuchs <weizenfuchs@gmail.com>
2026-01-19 22:47:32 +0100

    Remove unnecessary routing module and properly import the dashboard component into the app Enhance Readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # CargoLog
 Angular PWA zur Protokollierung von Anlieferungen
+
+# Setup:
+
+### Abhängigkeiten installieren:
+```
+npm install
+```
+### Development Server starten:
+```
+ng serve
+```
+
+### Im Browser aufrufen:
+```
+http://localhost:4200
+```
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,17 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard';
-import { AppRoutingModule } from './app-routing.module';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   imports: [
     RouterOutlet,
     Dashboard,
-    AppRoutingModule,
   ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
 })
 export class App {
   protected readonly title = signal('cargolog');
