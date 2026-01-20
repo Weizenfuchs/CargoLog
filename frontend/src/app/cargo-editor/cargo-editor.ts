26a1b8f0089daffd79e0233b0e934bb6ecbb21b0
Weizenfuchs <weizenfuchs@gmail.com>
2026-01-20 11:04:05 +0100

    Add error message for quantity input and prevent invalid key events

--- frontend/src/app/cargo-editor/cargo-editor.ts
+++ frontend/src/app/cargo-editor/cargo-editor.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-cargo-editor',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './cargo-editor.html',
   styleUrl: './cargo-editor.css',
 })
 export class CargoEditor {
-
+  amountEvent(event: KeyboardEvent): void {
+    if (event.key === '-' || event.key === '+') {
+      event.preventDefault();
+    }
+  }
 }
